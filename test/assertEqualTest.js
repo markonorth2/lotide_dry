cc8b990a77f71eba3247a745cb1d356eecdbcd1e
Mark Chuang <markchuangth@gmail.com>
2021-11-29 20:06:19 -0500

    first complete

---
+++ test/assertEqualTest.js
@@ -0,0 +1,9 @@
+const assertEqual = require('../assertEqual')
+
+
+
+
+// TEST CODE
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+console.log(assertEqual(1, 1));
+console.log(assertEqual(1, 2));
