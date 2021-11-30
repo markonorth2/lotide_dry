ae833ca24034687e6f07c5cd5248e5d3ac171966
Mark Chuang <markchuangth@gmail.com>
2021-11-29 19:59:47 -0500

    Using mocha and Chai instead of our own assertion functions

---
+++ test/headTest.js
@@ -0,0 +1,29 @@
+// Using our own assertion functions instead of a real testing framework
+
+  // const assertEqual = require('../assertEqual')
+  // const head = require('../head')
+
+  // console.log(assertEqual(head([5,6,7]), 5));
+  // console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
+  // console.log(assertEqual(head([]).length, 0));
+
+
+  // console.log(assertEqual(head([])[0], undefined));
+
+
+// Using Mocha and Chai 
+
+  const assert = require('chai').assert;
+  const head   = require('../head');
+
+  describe("#head", () => {
+    it("returns 1 for [1, 2, 3]", () => {
+      assert.strictEqual(head([1, 2, 3]), 1);
+    });
+    it("returns '5' for ['5']", () => {
+      assert.strictEqual(head(['5']), '5'); 
+    });
+
+  });
+
+  
